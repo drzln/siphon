8edb8b11a0596b2b07e63703eb10d861197586b9
drzln <drzzln@protonmail.com>
2026-09-20 16:14:31 -0300

    formatting

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Zero-Overhead eBPF OTLP Telemetry Daemon";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-ninja = {
+      url = "github:pdpartners/nix-ninja";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-ninja }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      buildEnvironment = with pkgs; [
+        protobuf-c
+        elfutils
+        bpftool
+        libbpf
+        clang
+        llvm
+        zlib
+      ];
+    in
+    {
+      packages.${system}.default = nix-ninja.lib.${system}.buildNinjaPackage {
+        pname = "siphon";
+        version = "0.1.0";
+        src = ./.;
+        ninjaFile = "./build.ninja";
+        nativeBuildInputs = buildEnvironment;
+      }
+    };
+  }
+}
