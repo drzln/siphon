83d2097981e4ec143ae83b56f91e864a928a61a1
drzzln <drzzln@protonmail.com>
2026-09-21 11:15:59 -0400

    even out

--- flake.nix
+++ flake.nix
@@ -9,29 +9,30 @@
     };
   };
 
-  outputs = { self, nixpkgs, nix-ninja }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    nixpkgs,
+    nix-ninja,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
 
-      buildEnvironment = with pkgs; [
-        protobuf-c
-        elfutils
-        bpftool
-        libbpf
-        clang
-        llvm
-        zlib
-      ];
-    in
-    {
-      packages.${system}.default = nix-ninja.lib.${system}.buildNinjaPackage {
-        pname = "siphon";
-        version = "0.1.0";
-        src = ./.;
-        ninjaFile = "./build.ninja";
-        nativeBuildInputs = buildEnvironment;
-      };
+    buildEnvironment = with pkgs; [
+      protobuf-c
+      elfutils
+      bpftool
+      libbpf
+      clang
+      llvm
+      zlib
+    ];
+  in {
+    packages.${system}.default = nix-ninja.lib.${system}.buildNinjaPackage {
+      pname = "siphon";
+      version = "0.1.0";
+      src = ./.;
+      ninjaFile = "./build.ninja";
+      nativeBuildInputs = buildEnvironment;
     };
   };
 }
